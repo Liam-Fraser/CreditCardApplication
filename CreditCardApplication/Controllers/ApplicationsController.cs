5c0104b0f7877d4bdd0692cd29a534ab2d991438
Liam Fraser <liamfraser@live.com>
2019-09-18 12:22:58 +0100

    Cleanup of un-needed Razor pages and auto-generated comments.

---
+++ CreditCardApplication/Controllers/ApplicationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CreditCardApplication.Controllers
+{
+    public class ApplicationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- CreditCardApplication/Controllers/ApplyController.cs
+++ CreditCardApplication/Controllers/ApplyController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using CreditCardApplication.Models;
 using CreditCardApplication.Services;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace CreditCardApplication.Controllers
 {
